e51a78c37147013fd0ef825dfc4378a2b7992f24
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2023-11-17 01:39:15 +0800

    fix yaml formatter not works

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -10,7 +10,7 @@ return {
     config.sources = {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.prettier,
       null_ls.builtins.formatting.clang_format.with {
         filetypes = { "c", "cpp", "objc", "objcpp", "cc", "cxx" },
       },
