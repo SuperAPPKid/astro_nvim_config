248558062fc50c1316aefa6c1bd149b6bbdfeca0
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-01-25 08:47:26 +0800

    add live-server

--- plugins/user.lua
+++ plugins/user.lua
@@ -956,4 +956,10 @@ return {
     dependencies = "neovim/nvim-lspconfig",
     event = "LspAttach",
   },
+
+  {
+    "aurum77/live-server.nvim",
+    build = function() require("live_server.util").install() end,
+    cmd = { "LiveServer", "LiveServerStart", "LiveServerStop" },
+  },
 }
