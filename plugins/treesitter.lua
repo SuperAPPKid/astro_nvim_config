e78ebbbdc5742e26c77e642d6cb8be03328d35da
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-02-05 03:13:55 +0800

    fix treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -33,35 +33,7 @@ return {
   end,
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      "arduino",
-      "cmake",
-      "csv",
-      "diff",
-      "dot",
-      "git_config",
-      "git_rebase",
-      "gitattributes",
-      "gitcommit",
-      "gitignore",
-      "godot_resource",
-      "gomod",
-      "gosum",
-      "gotmpl",
-      "gowork",
-      "graphql",
-      "jsdoc",
-      "luadoc",
-      "make",
-      "phpdoc",
-      "pod",
-      "regex",
-      "scss",
-      "sql",
-      "ssh_config",
-      "swift",
-      "xml",
-    })
+    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {})
 
     opts.textobjects.select.enabled = false
 
@@ -87,10 +59,6 @@ return {
     }
     opts.textobjects.swap.swap_previous = {
       ["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
-    }
-
-    opts.matchup = {
-      enable = true,
     }
 
     vim.treesitter.language.register("bash", "dotenv")
