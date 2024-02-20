9551e8e3fd48532c3506616c783065421e3ec8d8
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-02-21 03:09:55 +0800

    add php to treesitter ensure_installed

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -43,7 +43,7 @@ return {
     }
 
     -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {})
+    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, { "php" })
 
     opts.textobjects.select.enabled = false
 
