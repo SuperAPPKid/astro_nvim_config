6d3269d788417dc7bbbd131027be41a9447094fe
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-04-13 00:36:04 +0800

    migrate colors

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    { "NvChad/nvim-colorizer.lua", enabled = false },
+    {
+      "uga-rosa/ccc.nvim",
+      tag = "v1.7.2",
+      event = "User AstroFile",
+      cmd = { "CccPick", "CccConvert", "CccHighlighterEnable", "CccHighlighterDisable", "CccHighlighterToggle" },
+      dependencies = {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings
+          maps.n["<leader>zc"] = { "<cmd>CccConvert<cr>", desc = "Convert color" }
+          maps.n["<leader>zp"] = { "<cmd>CccPick<cr>", desc = "Pick Color" }
+        end,
+      },
+      config = function(_, opts)
+        require("ccc").setup(opts)
+
+        local highlighter = opts.highlighter
+        local action = function() vim.cmd.CccHighlighterToggle() end
+        if highlighter then
+          local flag = highlighter.auto_enable
+          local toggle = vim.schedule_wrap(function()
+            if flag then
+              vim.cmd.CccHighlighterEnable()
+            else
+              vim.cmd.CccHighlighterDisable()
+            end
+          end)
+          action = function()
+            flag = not flag
+            toggle()
+          end
+
+          vim.api.nvim_create_autocmd({ "BufEnter" }, {
+            callback = toggle,
+          })
+
+          toggle()
+        end
+
+        require("astrocore").set_mappings {
+          n = {
+            ["<leader>uC"] = {
+              action,
+              desc = "Toggle colorizer",
+            },
+          },
+        }
+      end,
+      opts = {
+        highlighter = {
+          auto_enable = true,
+          lsp = true,
+        },
+      },
+    },
+  },
+}
