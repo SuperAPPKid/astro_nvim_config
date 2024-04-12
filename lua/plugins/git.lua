b74de6491c3f691e271f949e2067b1e053f6f7ce
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-04-13 00:36:09 +0800

    migrate git

---
+++ lua/plugins/git.lua
@@ -0,0 +1,112 @@
+return {
+  {
+    "SuperBo/fugit2.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "nvim-lua/plenary.nvim",
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings
+          maps.n["<Leader>gf"] = { "<Cmd>Fugit2<CR>", desc = "Fugit2" }
+          maps.n["<leader>gG"] = { "<Cmd>Fugit2Graph<CR>", desc = "Fugit2 Graph" }
+        end,
+      },
+      {
+        "chrisgrieser/nvim-tinygit",
+        dependencies = {
+          "stevearc/dressing.nvim",
+          {
+            "AstroNvim/astrocore",
+            opts = function(_, opts)
+              local maps = opts.mappings
+              maps.n["<Leader>gm"] = {
+                function() require("tinygit").smartCommit() end,
+                desc = "New commit",
+              }
+              maps.n["<Leader>gP"] = {
+                function() require("tinygit").push { forceWithLease = true } end,
+                desc = "Push",
+              }
+            end,
+          },
+        },
+      },
+    },
+    cmd = { "Fugit2", "Fugit2Graph" },
+    opts = {
+      width = "80%",
+      height = "95%",
+    },
+  },
+
+  {
+    "sindrets/diffview.nvim",
+    event = "User AstroGitFile",
+    cmd = {
+      "DiffviewFileHistory",
+      "DiffviewOpen",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+      "DiffviewRefresh",
+    },
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local last_tabpage = vim.api.nvim_get_current_tabpage()
+          local find_target_tab = function(cmd)
+            local lib = require "diffview.lib"
+            local view = lib.get_current_view()
+            if view then
+              -- Current tabpage is a Diffview: go to previous tabpage
+              vim.api.nvim_set_current_tabpage(last_tabpage)
+            else
+              for _, v in ipairs(lib.views) do
+                local tabn = vim.api.nvim_tabpage_get_number(v.tabpage)
+                vim.cmd.tabclose(tabn)
+              end
+
+              last_tabpage = vim.api.nvim_get_current_tabpage()
+
+              vim.cmd(cmd)
+            end
+          end
+
+          local maps = opts.mappings
+          local prefix = "<leader>g"
+          maps.n[prefix .. "d"] = {
+            function() find_target_tab "DiffviewOpen -uno -- %" end,
+            desc = "Open File Diff",
+          }
+          maps.n[prefix .. "D"] = {
+            function() find_target_tab "DiffviewOpen -uno -- %" end,
+            desc = "Open File Diff",
+          }
+        end,
+      },
+    },
+    opts = {
+      hooks = {
+        view_opened = function() require("diffview.actions").toggle_files() end,
+      },
+    },
+  },
+
+  {
+    "almo7aya/openingh.nvim",
+    cmd = { "OpenInGHRepo", "OpenInGHFile", "OpenInGHFileLines" },
+    dependencies = {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = opts.mappings
+        local prefix = "<Leader>go"
+        maps.n[prefix] = { desc = "OpenInGH" }
+        maps.n[prefix .. "r"] = { "<Cmd>OpenInGHRepo<CR>", desc = "Open git repo in web" }
+        maps.n[prefix .. "f"] = { "<Cmd>OpenInGHFile<CR>", desc = "Open git file in web" }
+        maps.x[prefix .. "f"] = { "<Cmd>OpenInGHFileLines<CR>", desc = "Open git lines in web" }
+      end,
+    },
+  },
+}
