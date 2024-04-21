2a2c5a24b3bf30d3498e102b1b6557946abcfd82
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-04-22 02:47:51 +0800

    migrate tabline

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,16 +1,9 @@
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
-
----@type LazySpec
 return {
   "AstroNvim/astroui",
-  ---@type AstroUIOpts
   opts = {
-    -- change colorscheme
     colorscheme = "kanagawa",
-    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
         -- Normal = { bg = "#000000" },
@@ -19,9 +12,7 @@ return {
         -- Normal = { bg = "#000000" },
       },
     },
-    -- Icons can be configured throughout the interface
     icons = {
-      -- configure the loading of the lsp in the status line
       LSPLoading1 = "⠋",
       LSPLoading2 = "⠙",
       LSPLoading3 = "⠹",
@@ -32,6 +23,21 @@ return {
       LSPLoading8 = "⠧",
       LSPLoading9 = "⠇",
       LSPLoading10 = "⠏",
+    },
+    status = {
+      attributes = {
+        buffer_active = { bold = true, italic = false },
+        buffer_visible = { bold = true, italic = true },
+      },
+      colors = function(colors)
+        local get_hlgroup = require("astroui").get_hlgroup
+        local Comment = get_hlgroup "Comment"
+        local TabLineFill = get_hlgroup "TabLineFill"
+
+        colors.buffer_visible_fg = Comment.fg
+        colors.buffer_visible_bg = TabLineFill.bg
+        return colors
+      end,
     },
   },
 }
