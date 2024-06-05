4ba63a4963ae35ad89d6e119d8579590468ba442
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-06-06 02:06:54 +0800

    update `client/registerCapability` for lsp_handlers in astrolsp

--- lua/astro/astrolsp.lua
+++ lua/astro/astrolsp.lua
@@ -8,154 +8,167 @@ return {
   "AstroNvim/astrolsp",
   version = false,
   ---@type AstroLSPOpts
-  opts = {
-    -- Configuration table of features provided by AstroLSP
-    features = {
-      autoformat = true, -- enable or disable auto formatting on start
-      codelens = true, -- enable/disable codelens refresh on start
-      inlay_hints = true, -- enable/disable inlay hints on start
-      semantic_tokens = false, -- enable/disable semantic token highlighting
-    },
-    -- customize lsp formatting options
-    formatting = {
-      -- control auto formatting on save
-      format_on_save = {
-        enabled = true, -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
-        },
-        ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
-        },
-      },
-      disabled = { -- disable formatting capabilities for the listed language servers
-        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        -- "lua_ls",
-      },
-      timeout_ms = 1000, -- default format timeout
-      -- filter = function(client) -- fully override the default formatting function
-      --   return true
-      -- end
-    },
-    -- enable servers that you already have installed without mason
-    servers = {
-      -- "pyright"
-    },
-    -- Configure default capabilities for language servers (`:h vim.lsp.protocol.make_client.capabilities()`)
-    capabilities = {},
-    -- customize language server configuration options passed to `lspconfig`
-    ---@diagnostic disable: missing-fields
-    config = {
-      intelephense = {
-        handlers = {
-          ["textDocument/publishDiagnostics"] = function() end,
-        },
-      },
-      clangd = {
-        filetypes = { "c", "cpp", "opjc", "objcpp" },
-        capabilities = {
-          offsetEncoding = "utf-8",
-        },
-      },
-      gopls = {
-        settings = {
-          gopls = {
-            usePlaceholders = false,
-            hints = {
-              assignVariableTypes = true,
-              compositeLiteralFields = false,
-              compositeLiteralTypes = false,
-              constantValues = true,
-              functionTypeParameters = false,
-              parameterNames = true,
-              rangeVariableTypes = true,
-            },
-          },
-        },
-      },
-      tsserver = {
-        settings = {
-          typescript = {
-            inlayHints = {
-              includeInlayParameterNameHints = "all",
-              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-              includeInlayFunctionParameterTypeHints = true,
-              includeInlayVariableTypeHints = true,
-              includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-              includeInlayPropertyDeclarationTypeHints = true,
-              includeInlayFunctionLikeReturnTypeHints = true,
-              includeInlayEnumMemberValueHints = true,
-            },
-          },
-          javascript = {
-            inlayHints = {
-              includeInlayEnumMemberValueHints = true,
-              includeInlayFunctionLikeReturnTypeHints = true,
-              includeInlayFunctionParameterTypeHints = true,
-              includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-              includeInlayPropertyDeclarationTypeHints = true,
-              includeInlayVariableTypeHints = true,
-            },
-          },
-        },
-      },
-    },
-    -- customize how language servers are attached
-    handlers = {
-      -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
-      -- function(server, opts) require("lspconfig")[server].setup(opts) end
+  opts = function(_, opts)
+    local register_capability_handler = vim.lsp.handlers["client/registerCapability"]
 
-      -- the key is the server that is being setup with `lspconfig`
-      -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
-      -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
-    },
-    -- Configure buffer local auto commands to add when attaching a language server
-    autocmds = {
-      -- first key is the `augroup` to add the auto commands to (:h augroup)
-      lsp_document_highlight = {
-        -- Optional condition to create/delete auto command group
-        -- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
-        -- condition will be resolved for each client on each execution and if it ever fails for all clients,
-        -- the auto commands will be deleted for that buffer
-        cond = "textDocument/documentHighlight",
-        -- cond = function(client, bufnr) return client.name == "lua_ls" end,
-        -- list of auto commands to set
-        {
-          -- events to trigger
-          event = { "CursorHold", "CursorHoldI" },
-          -- the rest of the autocmd options (:h nvim_create_autocmd)
-          desc = "Document Highlighting",
-          callback = function()
-            if false then vim.lsp.buf.document_highlight() end
-          end,
+    local new_opts = {
+      -- Configuration table of features provided by AstroLSP
+      features = {
+        autoformat = true, -- enable or disable auto formatting on start
+        codelens = true, -- enable/disable codelens refresh on start
+        inlay_hints = true, -- enable/disable inlay hints on start
+        semantic_tokens = false, -- enable/disable semantic token highlighting
+      },
+      -- customize lsp formatting options
+      formatting = {
+        -- control auto formatting on save
+        format_on_save = {
+          enabled = true, -- enable or disable format on save globally
+          allow_filetypes = { -- enable format on save for specified filetypes only
+            -- "go",
+          },
+          ignore_filetypes = { -- disable format on save for specified filetypes
+            -- "python",
+          },
         },
-        {
-          event = { "CursorMoved", "CursorMovedI", "BufLeave" },
-          desc = "Document Highlighting Clear",
-          callback = function()
-            if false then return vim.lsp.buf.clear_references() end
-          end,
+        disabled = { -- disable formatting capabilities for the listed language servers
+          -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
+          -- "lua_ls",
+        },
+        timeout_ms = 1000, -- default format timeout
+        -- filter = function(client) -- fully override the default formatting function
+        --   return true
+        -- end
+      },
+      -- enable servers that you already have installed without mason
+      servers = {
+        -- "pyright"
+      },
+      -- Configure default capabilities for language servers (`:h vim.lsp.protocol.make_client.capabilities()`)
+      capabilities = {},
+      -- customize language server configuration options passed to `lspconfig`
+      ---@diagnostic disable: missing-fields
+      config = {
+        intelephense = {
+          handlers = {
+            ["textDocument/publishDiagnostics"] = function() end,
+          },
+        },
+        clangd = {
+          filetypes = { "c", "cpp", "opjc", "objcpp" },
+          capabilities = {
+            offsetEncoding = "utf-8",
+          },
+        },
+        gopls = {
+          settings = {
+            gopls = {
+              usePlaceholders = false,
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = false,
+                compositeLiteralTypes = false,
+                constantValues = true,
+                functionTypeParameters = false,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+            },
+          },
+        },
+        tsserver = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+              },
+            },
+          },
         },
       },
-    },
-    -- mappings to be set up on attaching of a language server
-    mappings = {
-      n = {
-        -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
-        -- gD = {
-        --   function() vim.lsp.buf.declaration() end,
-        --   desc = "Declaration of current symbol",
-        --   cond = "textDocument/declaration",
-        -- },
-        -- ["<Leader>uY"] = {
-        --   function() require("astrolsp.toggles").buffer_semantic_tokens() end,
-        --   desc = "Toggle LSP semantic highlight (buffer)",
-        --   cond = function(client) return client.server_capabilities.semanticTokensProvider and vim.lsp.semantic_tokens end,
-        -- },
+      -- customize how language servers are attached
+      handlers = {
+        -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
+        -- function(server, opts) require("lspconfig")[server].setup(opts) end
+
+        -- the key is the server that is being setup with `lspconfig`
+        -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
+        -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
       },
-    },
-    -- A custom `on_attach` function to be run after the default `on_attach` function
-    -- takes two parameters `client` and `bufnr`  (`:h lspconfig-setup`)
-  },
+      -- Configure buffer local auto commands to add when attaching a language server
+      autocmds = {
+        -- first key is the `augroup` to add the auto commands to (:h augroup)
+        lsp_document_highlight = {
+          -- Optional condition to create/delete auto command group
+          -- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
+          -- condition will be resolved for each client on each execution and if it ever fails for all clients,
+          -- the auto commands will be deleted for that buffer
+          cond = "textDocument/documentHighlight",
+          -- cond = function(client, bufnr) return client.name == "lua_ls" end,
+          -- list of auto commands to set
+          {
+            -- events to trigger
+            event = { "CursorHold", "CursorHoldI" },
+            -- the rest of the autocmd options (:h nvim_create_autocmd)
+            desc = "Document Highlighting",
+            callback = function()
+              if false then vim.lsp.buf.document_highlight() end
+            end,
+          },
+          {
+            event = { "CursorMoved", "CursorMovedI", "BufLeave" },
+            desc = "Document Highlighting Clear",
+            callback = function()
+              if false then return vim.lsp.buf.clear_references() end
+            end,
+          },
+        },
+      },
+      lsp_handlers = {
+        ["client/registerCapability"] = function(err, res, ctx)
+          local ret = register_capability_handler(err, res, ctx)
+          -- local attached_client = M.attached_clients[ctx.client_id]
+          -- if attached_client then
+          --   M.on_attach(attached_client, vim.api.nvim_get_current_buf())
+          -- end
+          return ret
+        end,
+      },
+      -- mappings to be set up on attaching of a language server
+      mappings = {
+        n = {
+          -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
+          -- gD = {
+          --   function() vim.lsp.buf.declaration() end,
+          --   desc = "Declaration of current symbol",
+          --   cond = "textDocument/declaration",
+          -- },
+          -- ["<Leader>uY"] = {
+          --   function() require("astrolsp.toggles").buffer_semantic_tokens() end,
+          --   desc = "Toggle LSP semantic highlight (buffer)",
+          --   cond = function(client) return client.server_capabilities.semanticTokensProvider and vim.lsp.semantic_tokens end,
+          -- },
+        },
+      },
+    }
+    return require("astrocore").extend_tbl(opts, new_opts)
+  end,
 }
