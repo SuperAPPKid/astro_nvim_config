b372954ba56e41d9d6fef4fcadd93ecca1788363
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-06-19 22:47:42 +0800

    purge `persistent-breakpoints.nvim`

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1030,36 +1030,6 @@ return {
   },
 
   {
-    "Weissle/persistent-breakpoints.nvim",
-    event = "BufReadPost",
-    opts = function(_, opts)
-      return require("astrocore").extend_tbl(opts, {
-        load_breakpoints_event = { "BufReadPost" },
-      })
-    end,
-    keys = {
-      {
-        "<Leader>db",
-        function() require("persistent-breakpoints.api").toggle_breakpoint() end,
-        { silent = true },
-        desc = "Toggle Breakpoint",
-      },
-      {
-        "<Leader>dd",
-        function() require("persistent-breakpoints.api").clear_all_breakpoints() end,
-        { silent = true },
-        desc = "Clear Breakpoints",
-      },
-      {
-        "<Leader>dC",
-        function() require("persistent-breakpoints.api").set_conditional_breakpoint() end,
-        { silent = true },
-        desc = "Conditional Breakpoint",
-      },
-    },
-  },
-
-  {
     "kevinhwang91/nvim-hlslens",
     -- dependencies = { "AstroNvim/astrocore", opts = { on_keys = { auto_hlsearch = false } } },
     event = "BufRead",
