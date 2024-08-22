5d2d8848e9a88e4e324d05eca89220528c7a3b8d
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-08-22 23:50:53 +0800

    update gitgraph settings

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -72,8 +72,14 @@ return {
         optional = true,
         opts = {
           hooks = {
-            on_select_commit = function(commit) vim.cmd.DiffviewOpen(commit.hash .. "^!") end,
-            on_select_range_commit = function(from, to) vim.cmd.DiffviewOpen(from.hash .. "~1.." .. to.hash) end,
+            on_select_commit = function(commit)
+              vim.cmd "bwipeout!"
+              vim.cmd.DiffviewOpen(commit.hash .. "^!")
+            end,
+            on_select_range_commit = function(from, to)
+              vim.cmd "bwipeout!"
+              vim.cmd.DiffviewOpen(from.hash .. "~1.." .. to.hash)
+            end,
           },
         },
       },
@@ -374,6 +380,26 @@ return {
       { -- mapping to open GitGraph
         "AstroNvim/astrocore",
         opts = {
+          autocmds = {
+            gitgraph_settings = {
+              {
+                event = "FileType",
+                desc = "Add quit keymap for gitgraph",
+                pattern = "gitgraph",
+                callback = function(args)
+                  require("astrocore").set_mappings({
+                    n = {
+                      q = {
+                        "<Cmd>bwipeout!<CR>",
+                        noremap = true,
+                        silent = true,
+                      },
+                    },
+                  }, { buffer = args.buf })
+                end,
+              },
+            },
+          },
           mappings = {
             n = {
               ["<Leader>g|"] = {
