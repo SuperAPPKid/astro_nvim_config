b8a3f927607b1d16fec4e51235ec691d293d7e02
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-09-23 00:11:38 +0800

    update grug-far.nvim

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -1,12 +1,41 @@
-return {
+local default_opts = { instanceName = "main", transient = true }
+local function grug_far_open(opts, with_visual)
+  local grug_far = require "grug-far"
+  opts = require("astrocore").extend_tbl(default_opts, opts)
+  if not grug_far.has_instance(opts.instanceName) then
+    grug_far.open(opts)
+  else
+    if with_visual then
+      if not opts.prefills then opts.prefills = {} end
+      opts.prefills.search = grug_far.get_current_visual_selection()
+    end
+    grug_far.open_instance(opts.instanceName)
+    if opts.prefills then grug_far.update_instance_prefills(opts.instanceName, opts.prefills, false) end
+  end
+end
 
+--@type LazySpec
+return {
   {
     "MagicDuck/grug-far.nvim",
     cmd = "GrugFar",
-    opts = {
-      startInInsertMode = false,
-      transient = true,
-      keymaps = {
+    ---@param opts GrugFarOptionsOverride
+    opts = function(_, opts)
+      opts.icons = opts.icons or {}
+      opts.icons.enabled = vim.g.icons_enabled
+      if not vim.g.icons_enabled then
+        opts.resultsSeparatorLineChar = "-"
+        opts.spinnerStates = {
+          "|",
+          "\\",
+          "-",
+          "/",
+        }
+      end
+
+      opts.startInInsertMode = false
+      opts.transient = true
+      opts.keymaps = {
         replace = { n = "<localleader>r" },
         qflist = { n = "<localleader>q" },
         syncLocations = { n = "<localleader>s" },
@@ -16,44 +45,99 @@ return {
         historyAdd = { n = "<localleader>a" },
         refresh = { n = "<localleader>f" },
         openLocation = { n = "<localleader>o" },
+        openNextLocation = { n = "<Tab>" },
+        openPrevLocation = { n = "<S-Tab>" },
         gotoLocation = { n = "<enter>" },
         pickHistoryEntry = { n = "<enter>" },
         abort = { n = "<localleader>b" },
         help = { n = "g?" },
-        toggleShowRgCommand = { n = "<localleader>p" },
-      },
-    },
+        toggleShowCommand = { n = "<localleader>p" },
+        swapEngine = { n = "<localleader>e" },
+        previewLocation = { n = "<localleader>i" },
+        swapReplacementInterpreter = { n = "<localleader>x" },
+      }
+    end,
     dependencies = {
       {
         "AstroNvim/astrocore",
         opts = function(_, opts)
           local maps = opts.mappings
           maps.n["<Leader>zs"] = {
-            function() require("grug-far").open {} end,
+            function() grug_far_open() end,
             desc = "Search / Replace",
           }
           maps.n["<Leader>zS"] = {
             function()
-              require("grug-far").open {
-                prefills = { paths = vim.fn.expand "%" },
+              local ext = require("astrocore.buffer").is_valid() and vim.fn.expand "%:e" or ""
+              local paths = require("astrocore.buffer").is_valid() and vim.fn.expand "%" or nil
+              grug_far_open {
+                prefills = {
+                  paths = paths,
+                  filesFilter = ext ~= "" and "*." .. ext or nil,
+                },
               }
             end,
             desc = "Search / Replace (current file)",
           }
           maps.x["<Leader>zs"] = {
-            function() require("grug-far").open { startCursorRow = 4 } end,
+            function() grug_far_open({ startCursorRow = 4 }, true) end,
             desc = "Search / Replace",
           }
           maps.x["<Leader>zS"] = {
             function()
-              require("grug-far").with_visual_selection {
+              local ext = require("astrocore.buffer").is_valid() and vim.fn.expand "%:e" or ""
+              local paths = require("astrocore.buffer").is_valid() and vim.fn.expand "%" or nil
+              grug_far_open({
                 startCursorRow = 4,
-                prefills = { paths = vim.fn.expand "%" },
-              }
+                prefills = {
+                  paths = paths,
+                  filesFilter = ext ~= "" and "*." .. ext or nil,
+                },
+              }, true)
             end,
             desc = "Search / Replace (current file)",
           }
         end,
+      },
+    },
+    specs = {
+      {
+        "folke/which-key.nvim",
+        opts = function(_, opts)
+          if not opts.disable then opts.disable = {} end
+          opts.disable.ft = require("astrocore").list_insert_unique(opts.disable.ft, { "grug-far" })
+        end,
+      },
+      {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = {
+          commands = {
+            grug_far_replace = function(state)
+              local node = state.tree:get_node()
+              grug_far_open {
+                prefills = {
+                  paths = node.type == "directory" and node:get_id() or vim.fn.fnamemodify(node:get_id(), ":h"),
+                },
+              }
+            end,
+          },
+          window = {
+            mappings = {
+              gS = "grug_far_replace",
+            },
+          },
+        },
+      },
+      {
+        "stevearc/oil.nvim",
+        opts = {
+          keymaps = {
+            gS = {
+              function() grug_far_open { prefills = { paths = require("oil").get_current_dir() } } end,
+              desc = "Search/Replace in directory",
+            },
+          },
+        },
       },
     },
   },
