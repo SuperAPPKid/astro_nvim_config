b367661e143bf06646e22414b9a3dafd96604dab
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-10-02 16:15:35 +0800

    lock codeium.nvim version

--- lua/plugins/builtin.lua
+++ lua/plugins/builtin.lua
@@ -82,6 +82,7 @@ return {
       },
       {
         "Exafunction/codeium.nvim",
+        commit = "937667b2cadc7905e6b9ba18ecf84694cf227567",
         dependencies = {
           "nvim-lua/plenary.nvim",
         },
