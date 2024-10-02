fbddcf2334aed843c47f809b41f821a136de4474
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-10-03 04:11:16 +0800

    remove maplocalleader

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -5,7 +5,7 @@ require("lazy").setup({
     import = "astronvim.plugins",
     opts = { -- AstroNvim options must be set here with the `import` key
       mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
-      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
+      maplocalleader = "", -- This ensures the localleader key must be configured before Lazy is set up
       icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
       pin_plugins = false, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
     },
