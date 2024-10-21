b2db795ae9fa44b08d4efd0ba5259cde3b7a038b
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-10-22 00:00:05 +0800

    make toggleterm-manager loading lazily

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -2,40 +2,62 @@ return {
   {
     "ryanmsnyder/toggleterm-manager.nvim",
     lazy = true,
-    init = function(plugin) require("astrocore").on_load("telescope.nvim", plugin.name) end,
     dependencies = {
       "nvim-telescope/telescope.nvim",
       "nvim-lua/plenary.nvim",
       {
         "AstroNvim/astrocore",
         opts = function(_, opts)
-          opts.mappings.n["<Leader>tF"] = { "<Cmd>Telescope toggleterm_manager<CR>", desc = "Search Toggleterms" }
+          opts.mappings.n["<Leader>fs"] = { "<Cmd>Telescope toggleterm_manager<CR>", desc = "Search Toggleterms" }
         end,
       },
     },
+    config = function(_, opts)
+      require("toggleterm-manager").setup(opts)
+      require("telescope").load_extension "toggleterm_manager"
+    end,
     opts = function(_, opts)
       local term_icon = require("astroui").get_icon "Terminal"
-      local toggleterm_manager = require "toggleterm-manager"
-      local actions = toggleterm_manager.actions
-
-      return require("astrocore").extend_tbl(opts, {
-        titles = { prompt = term_icon .. " Terminals" },
-        results = { term_icon = term_icon },
-        mappings = {
-          n = {
-            ["<CR>"] = { action = actions.toggle_term, exit_on_action = true }, -- toggles terminal open/closed
-            ["r"] = { action = actions.rename_term, exit_on_action = false }, -- provides a prompt to rename a terminal
-            ["d"] = { action = actions.delete_term, exit_on_action = false }, -- deletes a terminal buffer
-            ["n"] = { action = actions.create_term, exit_on_action = false }, -- creates a new terminal buffer
+      opts.titles = { prompt = term_icon .. " Terminals" }
+      opts.results = { term_icon = term_icon }
+      opts.mappings = {
+        n = {
+          ["<CR>"] = { -- toggles terminal open/closed
+            action = function(...) require("toggleterm-manager").actions.toggle_term(...) end,
+            exit_on_action = true,
           },
-          i = {
-            ["<CR>"] = { action = actions.toggle_term, exit_on_action = true }, -- toggles terminal open/closed
-            ["<C-r>"] = { action = actions.rename_term, exit_on_action = false }, -- provides a prompt to rename a terminal
-            ["<C-d>"] = { action = actions.delete_term, exit_on_action = false }, -- deletes a terminal buffer
-            ["<C-n>"] = { action = actions.create_term, exit_on_action = false }, -- creates a new terminal buffer
+          ["<C-r>"] = { -- provides a prompt to rename a terminal
+            action = function(...) require("toggleterm-manager").actions.rename_term(...) end,
+            exit_on_action = false,
+          },
+          ["<C-d>"] = { -- deletes a terminal buffer
+            action = function(...) require("toggleterm-manager").actions.delete_term(...) end,
+            exit_on_action = false,
+          },
+          ["<C-n>"] = { -- creates a new terminal buffer
+            action = function(...) require("toggleterm-manager").actions.create_term(...) end,
+            exit_on_action = false,
           },
         },
-      })
+        i = {
+          ["<CR>"] = { -- toggles terminal open/closed
+            action = function(...) require("toggleterm-manager").actions.toggle_term(...) end,
+            exit_on_action = true,
+          },
+          ["<C-r>"] = { -- provides a prompt to rename a terminal
+            action = function(...) require("toggleterm-manager").actions.rename_term(...) end,
+            exit_on_action = false,
+          },
+          ["<C-d>"] = { -- deletes a terminal buffer
+            action = function(...) require("toggleterm-manager").actions.delete_term(...) end,
+            exit_on_action = false,
+          },
+          ["<C-n>"] = { -- creates a new terminal buffer
+            action = function(...) require("toggleterm-manager").actions.create_term(...) end,
+            exit_on_action = false,
+          },
+        },
+      }
     end,
   },
 
