0621b3eba71a17c810f953229d4712eac088c630
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2024-12-22 21:54:20 +0800

    remove molten-nvim

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1299,28 +1299,6 @@ return {
   },
 
   {
-    "benlubas/molten-nvim",
-    lazy = true,
-    version = "^1", -- use version <2.0.0 to avoid breaking changes
-    build = ":UpdateRemotePlugins",
-    specs = {
-      "AstroNvim/astrocore",
-      opts = function(_, opts)
-        local prefix = "<leader>k"
-
-        opts.mappings.n[prefix] = {
-          desc = require("astroui").get_icon("planet", 1, true) .. "Kernel",
-        }
-        opts.mappings.n[prefix .. "mi"] = { "<Cmd>MoltenInit<CR>", desc = "Initialize the plugin" }
-        opts.mappings.n[prefix .. "e"] = { "<Cmd>MoltenEvaluateOperator<CR>", desc = "Run operator selection" }
-        opts.mappings.n[prefix .. "rl"] = { "<Cmd>MoltenEvaluateLine<CR>", desc = "Evaluate line" }
-        opts.mappings.n[prefix .. "rr"] = { "<Cmd>MoltenReevaluateCell<CR>", desc = "Re-evaluate cell" }
-        opts.mappings.v[prefix .. "r"] = { ":<C-u>MoltenEvaluateVisual<CR>gv", desc = "Evaluate visual selection" }
-      end,
-    },
-  },
-
-  {
     "johmsalas/text-case.nvim",
     keys = {
       { "<Leader>zt", "<Cmd>TextCaseOpenTelescope<CR>", desc = "TextCase" },
