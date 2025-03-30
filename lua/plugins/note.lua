3452b15a9a0a3e11db7a6a3c787203b15de3903f
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2025-03-31 03:57:25 +0800

    add obsidian.nvim

--- lua/plugins/note.lua
+++ lua/plugins/note.lua
@@ -37,7 +37,7 @@ local M = {
     },
     opts = {
       title = " Global note ",
-      directory = "~/Documents/Notes",
+      directory = vim.env.HOME .. "/Documents/Notes",
       additional_presets = {
         project_local = {
           command_name = "ProjectNote",
@@ -68,6 +68,70 @@ local M = {
           col = 0,
         }
       end,
+    },
+  },
+
+  {
+    "epwalsh/obsidian.nvim",
+    -- the obsidian vault in this default config  ~/obsidian-vault
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand':
+    -- event = { "bufreadpre " .. vim.fn.expand "~" .. "/my-vault/**.md" },
+    event = { "BufReadPre  */Obsidian/*.md" },
+    init = function(plugin)
+      -- create dir if opts.dir not exist
+      local plugin_opts = require("lazy.core.plugin").values(plugin, "opts", false)
+      if not (vim.uv or vim.loop).fs_stat(plugin_opts.dir) then vim.fn.mkdir(plugin_opts.dir, "p") end
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function(_, opts)
+      require("obsidian").setup(opts)
+      require("astrocore").set_mappings {
+        n = {
+          ["gf"] = {
+            function()
+              if require("obsidian").util.cursor_on_markdown_link() then
+                return "<Cmd>ObsidianFollowLink<CR>"
+              else
+                return "gf"
+              end
+            end,
+            desc = "Obsidian Follow Link",
+            expr = true,
+          },
+        },
+      }
+    end,
+    opts = {
+      dir = vim.env.HOME .. "/Documents/Obsidian", -- specify the vault location. no need to call 'vim.fn.expand' here
+      use_advanced_uri = true,
+      finder = "telescope.nvim",
+
+      templates = {
+        subdir = "templates",
+        date_format = "%Y-%m-%d-%a",
+        time_format = "%H:%M",
+      },
+
+      note_frontmatter_func = function(note)
+        -- This is equivalent to the default frontmatter function.
+        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+        -- `note.metadata` contains any manually added fields in the frontmatter.
+        -- So here we just make sure those fields are kept in the frontmatter.
+        if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+        return out
+      end,
+
+      -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+      -- URL it will be ignored but you can customize this behavior here.
+      follow_url_func = vim.ui.open or function(url) require("astrocore").system_open(url) end,
     },
   },
 
