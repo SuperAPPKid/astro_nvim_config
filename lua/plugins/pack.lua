2d94fb61d0cff677e841e3c48bf2ff9d4da9e83d
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2025-04-07 02:11:47 +0800

    update omni-sharp settings

--- lua/plugins/pack.lua
+++ lua/plugins/pack.lua
@@ -15,14 +15,10 @@ return {
           config = {
             omnisharp = {
               handlers = {
-                ["textDocument/definition"] = function(...) require("omnisharp_extended").definition_handler(...) end,
-                ["textDocument/typeDefinition"] = function(...)
-                  require("omnisharp_extended").type_definition_handler(...)
-                end,
-                ["textDocument/references"] = function(...) require("omnisharp_extended").references_handler(...) end,
-                ["textDocument/implementation"] = function(...)
-                  require("omnisharp_extended").implementation_handler(...)
-                end,
+                ["textDocument/definition"] = function() require("omnisharp_extended").definition_handler() end,
+                ["textDocument/typeDefinition"] = function() require("omnisharp_extended").type_definition_handler() end,
+                ["textDocument/references"] = function() require("omnisharp_extended").references_handler() end,
+                ["textDocument/implementation"] = function() require("omnisharp_extended").implementation_handler() end,
               },
             },
           },
