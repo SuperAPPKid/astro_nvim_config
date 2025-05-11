a05fac4667ae7e6380fc745852b8e7bb50d826ea
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2025-05-12 02:20:45 +0800

    enhance godot file handling and plugin setup

--- lua/plugins/packs/godot.lua
+++ lua/plugins/packs/godot.lua
@@ -1,3 +1,19 @@
+local function gd_move_handler(source, destination)
+  if not vim.fs.root(source, "project.godot") then return end -- only run in godot projects
+  local suffix = source:match "%.gd$" and "uid" or "import"
+  local tmp_file = ("%s.%s"):format(source, suffix)
+  local tmp_stat = vim.uv.fs_stat(tmp_file)
+
+  if tmp_stat then
+    -- Rename the temporary file
+    local new_tmp_file = ("%s.%s"):format(destination, suffix)
+    local success, err = vim.uv.fs_rename(tmp_file, new_tmp_file)
+    if not success then
+      vim.notify(("Failed to rename Godot file: `%s`\n```\n%s\n```"):format(tmp_file, err or "unknown error"))
+    end
+  end
+end
+
 ---@type LazySpec
 return {
   {
@@ -31,22 +47,58 @@ return {
     ft = { "gdshader", "gdshaderinc" },
     cmd = { "GodotRun", "GodotRunLast", "GodotStart" },
     dependencies = {
-      { "hrsh7th/nvim-cmp" },
+      {
+        "Saghen/blink.cmp",
+        opts = {
+          sources = {
+            per_filetype = {
+              gdshader = { "quickgd" },
+              gdshaderinc = { "quickgd" },
+            },
+            providers = {
+              quickgd = { name = "quickgd", module = "quickgd.blink", score_offset = 1000 },
+            },
+          },
+        },
+      },
     },
     opts = function(_, opts)
       local is_available = require("astrocore").is_available
-      if is_available "nvim-cmp" then
-        opts.cmp = true
-        local cmp = require "cmp"
-        local sources = require("astrocore").list_insert_unique(
-          require("cmp.config").global.sources,
-          { { name = "quickgd", priority = 750 } }
-        )
-        dd(sources)
-        cmp.setup.filetype({ "gdshader", "gdshaderinc" }, { sources = sources })
-      end
+      opts.cmp = is_available "blink.cmp"
       opts.telescope = is_available "telescope.nvim"
       opts.treesitter = is_available "nvim-treesitter"
     end,
+  },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      event_handlers = {
+        {
+          event = "file_renamed",
+          handler = function(args) gd_move_handler(args.source, args.destination) end,
+          id = "godot_handler",
+        },
+        {
+          event = "file_moved",
+          handler = function(args) gd_move_handler(args.source, args.destination) end,
+          id = "godot_handler",
+        },
+      },
+      nesting_rules = {
+        godot_import = {
+          pattern = "^(.*)$",
+          files = { -- pretty loose, but is probably a specific enough pattern to be alright
+            "%1.import",
+          },
+        },
+        godot_uid = {
+          pattern = "^(.*%.gd)$",
+          files = {
+            "%1.uid",
+          },
+        },
+      },
+    },
   },
 }
