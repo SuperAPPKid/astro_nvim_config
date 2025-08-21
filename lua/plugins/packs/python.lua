3512c60bae6c948a2c54041eeae631ab62b9c1eb
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2025-08-21 17:06:03 +0800

    update python pack

--- lua/plugins/packs/python.lua
+++ lua/plugins/packs/python.lua
@@ -5,16 +5,8 @@ return {
     ft = "python",
     dependencies = "mfussenegger/nvim-dap",
     config = function(_, opts)
-      local path = vim.fn.exepath "python"
-      local debugpy = require("mason-registry").get_package "debugpy"
-      if debugpy:is_installed() then
-        path = vim.fn.expand "$MASON/packages/debugpy"
-        if vim.fn.has "win32" == 1 then
-          path = path .. "/venv/Scripts/python"
-        else
-          path = path .. "/venv/bin/python"
-        end
-      end
+      local path = vim.fn.exepath "debugpy-adapter"
+      if path == "" then path = vim.fn.exepath "python" end
       require("dap-python").setup(path, opts)
     end,
   },
