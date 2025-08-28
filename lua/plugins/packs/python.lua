f1f82a77a0459e83612a5e88428b89dfb5ccb703
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2025-08-28 19:20:45 +0800

    update python pack

--- lua/plugins/packs/python.lua
+++ lua/plugins/packs/python.lua
@@ -13,16 +13,23 @@ return {
 
   {
     "linux-cultist/venv-selector.nvim",
-    branch = "regexp",
     cmd = "VenvSelect",
     ft = "python",
     enabled = vim.fn.executable "fd" == 1 or vim.fn.executable "fdfind" == 1 or vim.fn.executable "fd-find" == 1,
-    opts = function(_, _)
-      require("astrocore").set_mappings {
-        n = {
-          ["<Leader>lv"] = { "<Cmd>VenvSelect<CR>", desc = "Select VirtualEnv" },
-        },
-      }
+    init = function(_)
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("venv_mappings", { clear = true }),
+        desc = "Add map for venv",
+        pattern = "python",
+        callback = function(args)
+          require("astrocore").set_mappings({
+            n = {
+              ["<Leader>zz"] = { "<Cmd>VenvSelect<CR>", desc = "Select VirtualEnv" },
+            },
+          }, { buffer = args.buf })
+        end,
+      })
     end,
+    config = true,
   },
 }
