900a0695619ac0c13575c354316c327ba74cdb9b
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2025-09-03 20:03:39 +0800

    fix dart pack bugs

--- lua/plugins/packs/dart.lua
+++ lua/plugins/packs/dart.lua
@@ -1,8 +1,9 @@
+local lsp_opts
 ---@type LazySpec
 return {
   {
     "akinsho/flutter-tools.nvim",
-    lazy = true,
+    ft = "dart",
     dependencies = {
       { "nvim-telescope/telescope.nvim" },
       {
@@ -13,38 +14,7 @@ return {
           opts.handlers.dartls = function(server, handler_opts)
             handler_opts.autostart = false
             default_handler(server, handler_opts)
-
-            local lsp_opts = require("astrocore").extend_tbl(opts.config.dartls, handler_opts)
-            local setup = function()
-              local path = require "flutter-tools.utils.path"
-              vim.opt_local.comments = [[sO:*\ -,mO:*\ \ ,exO:*/,s1:/*,mb:*,ex:*/,:///,://]]
-              vim.opt_local.commentstring = [[//%s]]
-              vim.opt.includeexpr = "v:lua.require('flutter-tools.resolve_url').resolve_url(v:fname)"
-              local full_path = vim.fn.expand "%:p"
-              -- Prevent writes to files in the pub cache and FVM folder.
-              if path.is_flutter_dependency_path(full_path) then vim.opt_local.modifiable = false end
-
-              require("flutter-tools").setup {
-                lsp = lsp_opts,
-                debugger = { enabled = true },
-                ui = { border = "double" },
-                widget_guides = { enabled = true },
-                dev_log = { enabled = false },
-              }
-              require("telescope").load_extension "flutter"
-              require("flutter-tools.lsp").attach()
-            end
-
-            if vim.bo.filetype == "dart" then
-              setup()
-            else
-              vim.api.nvim_create_autocmd("FileType", {
-                desc = "Attach flutter lsp",
-                pattern = "dart",
-                callback = function() setup() end,
-                once = true,
-              })
-            end
+            lsp_opts = handler_opts
           end
         end,
       },
@@ -63,7 +33,30 @@ return {
         end,
       })
     end,
-    config = function() end,
+    config = function(_, opts)
+      local path = require "flutter-tools.utils.path"
+      vim.opt_local.comments = [[sO:*\ -,mO:*\ \ ,exO:*/,s1:/*,mb:*,ex:*/,:///,://]]
+      vim.opt_local.commentstring = [[//%s]]
+      vim.opt.includeexpr = "v:lua.require('flutter-tools.resolve_url').resolve_url(v:fname)"
+      local full_path = vim.fn.expand "%:p"
+      -- Prevent writes to files in the pub cache and FVM folder.
+      if path.is_flutter_dependency_path(full_path) then vim.opt_local.modifiable = false end
+
+      if not lsp_opts then require("astrolsp").lsp_setup "dartls" end
+      opts = require("astrocore").extend_tbl(opts, { lsp = lsp_opts, debugger = { enabled = true } })
+      require("flutter-tools").setup(opts)
+      require("telescope").load_extension "flutter"
+
+      vim.api.nvim_create_autocmd("BufEnter", {
+        callback = function() require("flutter-tools.lsp").attach() end,
+        once = true,
+      })
+    end,
+    opts = {
+      ui = { border = "double" },
+      widget_guides = { enabled = true },
+      dev_log = { enabled = false },
+    },
   },
 
   {
